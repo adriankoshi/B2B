abbf2abfc221f469ae4e62c05a272f535dd04932
Adrian Koshi <adriankoshi@gmail.com>
2025-05-12 12:32:17 +0200

    Configure tailwind css and navbar design

--- b2b.client/src/components/UserList.jsx
+++ b2b.client/src/components/UserList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUsers, deleteUser } from "../services/userService";
 import { loginAsAdmin } from "../services/authService";
+import Navbar from "./partials/Navbar";
 
 export default function UserList() {
     const [users, setUsers] = useState([]);
@@ -32,27 +33,34 @@ export default function UserList() {
     };
 
     return (
-        <div>
-            <h2>User List</h2>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+        <>
+            <Navbar />
+            <div className="relative">
 
-            <button onClick={() => navigate("/users/create")}>
-               Create User
-            </button>
+                <div>
+                    <h2>User List</h2>
+                    {error && <p style={{ color: "red" }}>{error}</p>}
 
-            <ul>
-                {users.map(u => (
-                    <li key={u.id}>
-                        {u.username} - {u.email}
-                        <button onClick={() => navigate(`/users/edit/${u.id}`)}>
-                            Edit
-                        </button>
-                        <button onClick={() => handleDelete(u.id)}>
-                            Delete
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                    <button onClick={() => navigate("/users/create")}>
+                        Create User
+                    </button>
+
+                    <ul>
+                        {users.map(u => (
+                            <li key={u.id}>
+                                {u.username} - {u.email}
+                                <button onClick={() => navigate(`/users/edit/${u.id}`)}>
+                                    Edit
+                                </button>
+                                <button onClick={() => handleDelete(u.id)}>
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+        
     );
 }
---
+++ b2b.client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/partials/Navbar";
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <div className="relative">
+                {/* Hero Section */}
+                <div className="hero relative h-screen w-full flex flex-col lg:flex-row items-center justify-center text-center bg-[url('/images/hero.png')] bg-cover bg-center bg-no-repeat">
+                    
+                </div>
+            </div>
+        </>
+    );
+}
